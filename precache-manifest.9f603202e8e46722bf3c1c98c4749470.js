541ff022953763ddc03d317ecb2c61dcf2cfb2b9
JuliaDzyuba <newcom1000.2017@gmail.com>
2020-07-24 09:33:01 +0300

    Updates

---
+++ precache-manifest.9f603202e8e46722bf3c1c98c4749470.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18028fbbafd433cff9f8bbaaa7347b3b",
+    "url": "/newsportal/index.html"
+  },
+  {
+    "revision": "1599a4726bdd524f69a4",
+    "url": "/newsportal/static/css/main.58a2235b.chunk.css"
+  },
+  {
+    "revision": "02b40274a49df2774d86",
+    "url": "/newsportal/static/js/2.a1048d8a.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/newsportal/static/js/2.a1048d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1599a4726bdd524f69a4",
+    "url": "/newsportal/static/js/main.7cf35515.chunk.js"
+  },
+  {
+    "revision": "6d7a162a00ee58ab8468",
+    "url": "/newsportal/static/js/runtime-main.b04f8ade.js"
+  },
+  {
+    "revision": "4c25d18059353799aff91dcdd8f19bb4",
+    "url": "/newsportal/static/media/facebook.4c25d180.png"
+  },
+  {
+    "revision": "d19eb960e5ea43dcfcfb50b80b80c0ab",
+    "url": "/newsportal/static/media/instagram.d19eb960.png"
+  },
+  {
+    "revision": "22921365701970d52c7455a6f28d5aa9",
+    "url": "/newsportal/static/media/location.22921365.png"
+  },
+  {
+    "revision": "2b367454edd11a31ddc5c2e80d2e51be",
+    "url": "/newsportal/static/media/mail.2b367454.png"
+  },
+  {
+    "revision": "cd11a20a3f4c308f24c20810880eea67",
+    "url": "/newsportal/static/media/pinterest.cd11a20a.png"
+  }
+]);
